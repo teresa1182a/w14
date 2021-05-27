a42ed17e445897bfef76cc5b216ceda968a1b9d6
teresa1182a <tku406190040@gmail.com>
2021-05-27 21:58:40 +0800

    add

--- sc.js
+++ sc.js
@@ -5,3 +5,7 @@ const mainMenu=document.querySelector('.main-nav .main-menu');
 p.addEventListener('click',function(){
     mainMenu.classList.toggle('show');
 });
+
+/*function changeImage() {
+    document.header.style.backgroundImage=' url("./img/page.png")';
+  }*/
